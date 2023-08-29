aaa4a6979726bb1ec5641e37a7a20747405e384a
zoro <zoro@pop-os.localdomain>
2023-08-29 17:23:35 +0530

    first commit

---
+++ app/api/blogs/route.ts
@@ -0,0 +1,12 @@
+export const GET =async (req:Request) => {
+    console.log('get')
+    // get a post by its id
+}
+export const PUT =async (req:Request) => {
+    console.log('put')
+    // update a post by its id
+}
+export const PUT =async (req:Request) => {
+    console.log('put')
+    // delete a post by its id
+}
